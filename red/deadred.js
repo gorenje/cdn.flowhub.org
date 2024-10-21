c82d51054fea6bfc18e4c6dc9cbaa99447491c12
Gerrit Riessen <gorenje@posteo.de>
2024-10-21 11:27:30 +0200

    yet another jsonata fix

--- red/deadred.js
+++ red/deadred.js
@@ -287,7 +287,7 @@ var DEADRED = (function() {
                             msg[rle.p] = JSON.parse(rle.to)
                         }
                         if ( rle.pt == "msg" && rle.tot == "jsonata" ) {
-                            msg[rle.p] = jsonata(rle.to.replace(/\$\$[.]/g,"$").replace(/msg[.]/g,"$")).evaluate({},msg)
+                            msg[rle.p] = jsonata(rle.to.replace(/\$\$[.]/g,"$").replace(/msg[.]/g,"$")).evaluate(msg,msg)
                         }
                         if ( rle.pt == "msg" && rle.tot == "msg" ) {
                             RED.utils.setMessageProperty(
@@ -774,7 +774,7 @@ var DEADRED = (function() {
                             break
 
                         case "jsonata":
-                            msg[prp.p] = jsonata(prp.v.replace(/\$\$[.]/g,"$").replace(/msg[.]/g,"$")).evaluate({},msg)
+                            msg[prp.p] = jsonata(prp.v.replace(/\$\$[.]/g,"$").replace(/msg[.]/g,"$")).evaluate(msg,msg)
                             break
 
                         case "str":
